fab41c36e7fafb13695ebd8e8abe08b04b0cdd07
Luther Calvin Riggs <luthercalvinriggsllc@gmail.com>
2020-07-24 17:10:32 -0400

    completed helloWorld, leapYears, removeFromArray, repeatString, reverseString, sumAll and tempConversion

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,24 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    
+    let i = 0, newString = '';
+    
+    let stringArray = word.split('');
+    while (i < word.length) {
+        let x = stringArray.pop();
+        newString += x;
 
+        i++;
+    }
+    
+    return newString;
 }
 
 module.exports = reverseString
+
+
+// also acceptable
+/* 
+
+    return word.split('').reverse().join('');
+
+*/
--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    
+    let sum = 0;
+    let a, b;
 
+    if (min < 0 || max < 0) {
+        return "ERROR";
+    } else if (typeof min !== "number" || typeof max !== "number") {
+        return "ERROR";
+    } else {
+        if (min < max) {
+            a = min;
+            b = max;
+        } else {
+            a = max;
+            b = min; 
+        }   
+    }
+
+    for(let i = a; i <= b; i++) {
+        sum += i;
+    }
+
+    return sum;
 }
 
 module.exports = sumAll
